86c3bdfed9c55d6a53d4ff4abe6fd5b5f982f49b
Abhijeet Singh <43774040+Abhijeet199@users.noreply.github.com>
2020-02-26 17:21:45 +0530

    first commit

---
+++ js/loading.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    document.querySelector('.loading').className += 'hidden';
+    document.querySelector('.ganesh').style.display = 'none';
+    document.querySelector('body').classList.remove('stop-scrolling')
+})
