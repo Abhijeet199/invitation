6cfe77b375e5e13396f501ac722fc0f75dfb6a87
Abhijeet Singh <43774040+Abhijeet199@users.noreply.github.com>
2020-03-01 13:29:05 +0530

    changing loading ganesh time

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,28 @@
+const gettimeinsec = () => {
+    let d = new Date();
+    hour = d.getHours() * 3600;
+    min = d.getMinutes() * 60;
+    sec = d.getSeconds();
+    total_sec = hour + min + sec;
+
+    return (total_sec);
+}
+
+before = gettimeinsec();
+
 window.addEventListener('load', () => {
-    document.querySelector('.loading').className += 'hidden';
-    document.querySelector('.ganesh').style.display = 'none';
-    document.querySelector('body').classList.remove('stop-scrolling')
+    after = gettimeinsec();
+    if (after - before > 4) {
+        document.querySelector('.loading').className += 'hidden';
+        document.querySelector('.ganesh').style.display = 'none';
+        document.querySelector('body').classList.remove('stop-scrolling');
+    }
+    else {
+        setTimeout(() => {
+            document.querySelector('.loading').className += 'hidden';
+            document.querySelector('.ganesh').style.display = 'none';
+            document.querySelector('body').classList.remove('stop-scrolling');
+        }, 4000);
+    }
+
 })
