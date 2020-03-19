1356332f25294f4f731b2f57ff47920c802acafb
Abhijeet Singh <43774040+Abhijeet199@users.noreply.github.com>
2020-03-19 23:36:45 +0530

    added tips

--- js/app.js
+++ js/app.js
@@ -14,3 +14,13 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var element = document.querySelector('.help');
+if (isMobile) {
+    element.style.display = 'flex';
+} else {
+    element.style.display = 'none';
+}
